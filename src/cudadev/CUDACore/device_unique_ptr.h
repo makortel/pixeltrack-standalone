5da345c8e79526072291d4126d59adf522238164
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-11-05 03:09:13 +0100

    Add make_unique overloads with Context

--- src/cudadev/CUDACore/device_unique_ptr.h
+++ src/cudadev/CUDACore/device_unique_ptr.h
@@ -82,6 +82,44 @@ namespace cms {
     template <typename T, typename... Args>
     typename device::impl::make_device_unique_selector<T>::bounded_array make_device_unique_uninitialized(Args &&...) =
         delete;
+
+    // With Context
+    template <typename T>
+    typename device::impl::make_device_unique_selector<T>::non_array make_device_unique(Context &ctx) {
+      static_assert(std::is_trivially_constructible<T>::value,
+                    "Allocating with non-trivial constructor on the device memory is not supported");
+      int dev = currentDevice();
+      auto mem = ctx.allocate_device(sizeof(T));
+      return typename device::impl::make_device_unique_selector<T>::non_array{reinterpret_cast<T *>(mem.release()),
+                                                                              mem.get_deleter()};
+    }
+
+    template <typename T>
+    typename device::impl::make_device_unique_selector<T>::unbounded_array make_device_unique(size_t n, Context &ctx) {
+      using element_type = typename std::remove_extent<T>::type;
+      static_assert(std::is_trivially_constructible<element_type>::value,
+                    "Allocating with non-trivial constructor on the device memory is not supported");
+      auto mem = ctx.allocate_device(n * sizeof(element_type));
+      return typename device::impl::make_device_unique_selector<T>::unbounded_array{
+          reinterpret_cast<element_type *>(mem.release()), mem.get_deleter()};
+    }
+
+    // No check for the trivial constructor, make it clear in the interface
+    template <typename T>
+    typename device::impl::make_device_unique_selector<T>::non_array make_device_unique_uninitialized(Context &ctx) {
+      auto mem = ctx.allocate_device(sizeof(T));
+      return typename device::impl::make_device_unique_selector<T>::non_array{reinterpret_cast<T *>(mem.release()),
+                                                                              mem.get_deleter()};
+    }
+
+    template <typename T>
+    typename device::impl::make_device_unique_selector<T>::unbounded_array make_device_unique_uninitialized(
+        size_t n, Context &ctx) {
+      using element_type = typename std::remove_extent<T>::type;
+      auto mem = ctx.allocate_device(n * sizeof(element_type));
+      return typename device::impl::make_device_unique_selector<T>::unbounded_array{
+          reinterpret_cast<element_type *>(mem.release()), mem.get_deleter()};
+    }
   }  // namespace cuda
 }  // namespace cms
 
